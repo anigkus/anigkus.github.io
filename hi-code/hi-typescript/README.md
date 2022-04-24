cbe97cd4b1d9c80c1daa0c827d7da8e1f70a63f3
anigkus <anigkus@gmail.com>
2022-04-24 16:36:34 +0800

    feat:(typescript) add README.md file

---
+++ hi-code/hi-typescript/README.md
@@ -0,0 +1,3 @@
+# Hi-TypeScript
+Learning to understand the syntax of the TypeScript development language by [Anigkus](https://github.com/anigkus)
+
--- hi-code/hi-typescript/app.js
+++ hi-code/hi-typescript/app.js
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+//https://www.tutorialspoint.com/vuejs/vuejs_environment_setup.htm
+//https://www.tutorialspoint.com/typescript/typescript_variables.htm
 let message = "Hello, App!";
 console.log(message);
